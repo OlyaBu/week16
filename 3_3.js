65696e72507fe21ad2ae13c7f2978ed30c71a513
OlyaBu <burasova1991@gmail.com>
2022-06-17 00:16:54 +1000

    урааа

--- 3_3.js
+++ 3_3.js
@@ -1,11 +1,4 @@
-/* 
-function sum(a, b) /* {} */
-sum(1, 3) // 4
-sum(4, -2) // 2
-sum(3, true) // ошибка
-sum(1, 'test') // ошибка */
+let arr = ['js', 'css', 'html'];
+alert arr([0])
 
-let a = '';
-let b= '';
-function sum(a,b){
 console.log();}
